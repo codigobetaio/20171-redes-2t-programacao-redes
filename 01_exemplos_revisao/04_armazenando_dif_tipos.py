d8cae4fcabb322289a63fff9fe1e15cf709c33e9
Marcos Oliveira de Souza <neomarcos@gmail.com>
2019-10-27 20:44:17 -0400

    Update 04_armazenando_dif_tipos.py
    ajustando para o padrão utilizado no python 3

--- 01_exemplos_revisao/04_armazenando_dif_tipos.py
+++ 01_exemplos_revisao/04_armazenando_dif_tipos.py
@@ -4,6 +4,6 @@ d={'a':1,'b':2}
 l=[1,2,3]
 arq=open('datafile.txt','w')
 arq.write(s+'\n')
-arq.write('%s,%s,%s\n' % (x,y,z))
+arq.write('{}, {}, {} \n'.format(x, y, z)) # using python3 pattern
 arq.write(str(l)+'$'+str(d)+'\n')
 arq.close()
